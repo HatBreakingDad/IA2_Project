76be2ea5330960d4d96d4cc79dd148bc043ed9c5
HatBreakingDad <48573593+HatBreakingDad@users.noreply.github.com>
2020-06-16 09:22:49 +1000

    Development of Instock and Category Sorter Begins
    I began by copying the previous html webpage and adapting it into a new file. I also had to create a new function on the server-side py file.

--- app.py
+++ app.py
@@ -13,24 +13,52 @@ def search():
             reader = csv.reader(file)
             for line in reader:
                 dataout.append(line)
-        return render_template('search.html', getdata=dataout, dataLength = 8, resultLength = -1)
+        return render_template('search.html', getdata=dataout, dataLength=8, resultLength=-1)
     else:
         requestResults = request.form['results']
         search = []
-        with open("GE_Data.csv", mode='r', encoding='latin-1') as file:
+        with open("GE_Data.csv", mode='r', encoding='utf-8-sig') as file:
             reader = csv.reader(file)
             search.append(next(reader))
             categoryList = []
             for line in reader:
                 if (requestResults.lower() in ''.join(line).lower()):
                     search.append(line)
-                returnCategory = line[len(line)  - 1].replace('product_cat-','').replace('-',' ').title().split(',')
+                returnCategory = line[len(
+                    line) - 1].replace('product_cat-', '').replace('-', ' ').title().split(',')
                 for i in range(0, len(returnCategory)):
                     if(returnCategory[i] not in categoryList):
                         categoryList.append(returnCategory[i])
             print(categoryList)
-        return render_template('search.html', getdata=search, dataLength = 8, resultLength=len(search) - 1)
+        return render_template('search.html', getdata=search, dataLength=8, resultLength=len(search) - 1)
 
+
+@app.route("/stock", methods=["POST", "GET"])
+def stock():
+    if request.method == 'GET':
+        """convert the csv into a basic html table format"""
+        dataout = []
+        with open("GE_Data.csv", mode='r', encoding='utf-8-sig') as file:
+            reader = csv.reader(file)
+            for line in reader:
+                dataout.append(line)
+        return render_template('stock.html', getdata=dataout)
+    # else:
+    #     requestResults = request.form['results']
+    #     search = []
+    #     with open("GE_Data.csv", mode='r', encoding='utf-8-sig') as file:
+    #         reader = csv.reader(file)
+    #         search.append(next(reader))
+    #         categoryList = []
+    #         for line in reader:
+    #             if (requestResults.lower() in ''.join(line).lower()):
+    #                 search.append(line)
+    #             returnCategory = line[len(line)  - 1].replace('product_cat-','').replace('-',' ').title().split(',')
+    #             for i in range(0, len(returnCategory)):
+    #                 if(returnCategory[i] not in categoryList):
+    #                     categoryList.append(returnCategory[i])
+    #         print(categoryList)
+    #     return render_template('search.html', getdata=search)
 
 # @app.route("/form", methods=["POST", "GET"])
 # def form():
