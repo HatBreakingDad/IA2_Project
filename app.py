db3b2caeba4a0e9e5fdb4bf4efa28ce6950e32bb
HatBreakingDad <48573593+HatBreakingDad@users.noreply.github.com>
2020-06-16 08:48:13 +1000

    Image displaying and category listing
    During this stage of development, I went on to optimise the csv file reader, I created a concept logo for the site and began developing the table for the page. I created a function that loaded the referenced images and put in a placeholder if they could not be found. This was done for the search results / search page. I also split up the categories that were found in each row and made them more readable.

--- app.py
+++ app.py
@@ -13,43 +13,47 @@ def search():
             reader = csv.reader(file)
             for line in reader:
                 dataout.append(line)
-        return render_template('search.html', getdata=dataout)
+        return render_template('search.html', getdata=dataout, dataLength = 8, resultLength = -1)
     else:
         requestResults = request.form['results']
-        dataout = []
-        with open("primeimagescsv", mode='r', encoding='latin-1') as file:
-            reader = csv.reader(file)
-            for line in reader:
-                dataout.append(line)
-        search = [[], []]
-        for item in dataout:
-            if (requestResults.lower() in ' '.join(item).lower()):
-                search.append(item)
-        return render_template('search.html', getdata=search)
-
-
-@app.route("/form", methods=["POST", "GET"])
-def form():
-    if request.method == 'GET':
-        """convert the csv into a basic html table format"""
-        dataout = []
-        with open("FirstFleet.csv", mode='r', encoding='latin-1') as file:
-            reader = csv.reader(file)
-            for line in reader:
-                dataout.append(line)
-        return render_template('form.html', getdata=dataout)
-    else:
-        requestResults = request.form['results']
-        dataout = []
-        with open("FirstFleet.csv", mode='r', encoding='latin-1') as file:
-            reader = csv.reader(file)
-            for line in reader:
-                dataout.append(line)
         search = []
-        for item in dataout[1:]:
-            if (requestResults.lower() in ' '.join(item).lower()):
-                search.append(item)
-        return render_template('form.html', getdata=search)
+        with open("GE_Data.csv", mode='r', encoding='latin-1') as file:
+            reader = csv.reader(file)
+            search.append(next(reader))
+            categoryList = []
+            for line in reader:
+                if (requestResults.lower() in ''.join(line).lower()):
+                    search.append(line)
+                returnCategory = line[len(line)  - 1].replace('product_cat-','').replace('-',' ').title().split(',')
+                for i in range(0, len(returnCategory)):
+                    if(returnCategory[i] not in categoryList):
+                        categoryList.append(returnCategory[i])
+            print(categoryList)
+        return render_template('search.html', getdata=search, dataLength = 8, resultLength=len(search) - 1)
+
+
+# @app.route("/form", methods=["POST", "GET"])
+# def form():
+#     if request.method == 'GET':
+#         """convert the csv into a basic html table format"""
+#         dataout = []
+#         with open("FirstFleet.csv", mode='r', encoding='latin-1') as file:
+#             reader = csv.reader(file)
+#             for line in reader:
+#                 dataout.append(line)
+#         return render_template('form.html', getdata=dataout)
+#     else:
+#         requestResults = request.form['results']
+#         dataout = []
+#         with open("FirstFleet.csv", mode='r', encoding='latin-1') as file:
+#             reader = csv.reader(file)
+#             for line in reader:
+#                 dataout.append(line)
+#         search = []
+#         for item in dataout[1:]:
+#             if (requestResults.lower() in ' '.join(item).lower()):
+#                 search.append(item)
+#         return render_template('form.html', getdata=search)
 
 
 if __name__ == "__main__":
--- run.bat
+++ run.bat
@@ -1,8 +1,3 @@
-cd C:\Users\25499\OneDrive - Brisbane Boys' College\School\I.T\Year 11\Term 3\Solution
-FLASK_APP=app.py 
-FLASK_ENV=development 
-flask run
-pause
-@echo Press Enter to run the program using the alternate method
+cd C:\Users\25499\Documents\Github\IA2_Project
 python app.py
 pause
