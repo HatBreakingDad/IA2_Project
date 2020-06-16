0b9417dc4fee08d43e1d9434bf0d533afb9929e1
HatBreakingDad <48573593+HatBreakingDad@users.noreply.github.com>
2020-06-17 08:04:58 +1000

    Optimisations, Readability and Quality of Life Improvements
    I began experimenting with making certain sections of my code more efficient by turning repeating blocks into functions. This optimised both the readability and the efficiency of my program. I also finished adding a more precise filtering method with the help of the created form, and I began to comment certain sections of my code to make it more readable.
    
    I also began working on the new editor function, which I had concepted as a simple form that would allow for simple methods of changing product properties

--- app.py
+++ app.py
@@ -4,6 +4,23 @@ import csv
 app = Flask(__name__)
 
 
+def getCSV():
+    dataout = []
+    with open("GE_Data.csv", mode='r', encoding='utf-8-sig') as file:
+        reader = csv.reader(file)
+        categoryList = []
+        for line in reader:
+            dataout.append(line)
+            returnCategory = line[len(
+                line) - 1].replace('product_cat-', '').replace('-', ' ').title().split(',')
+            for i in range(0, len(returnCategory)):
+                if(returnCategory[i] not in categoryList):
+                    categoryList.append(returnCategory[i])
+        return categoryList, dataout
+        # return categoryList
+        # return dataout
+
+
 @app.route("/search", methods=["POST", "GET"])
 def search():
     if request.method == 'GET':
@@ -19,6 +36,9 @@ def search():
                 for i in range(0, len(returnCategory)):
                     if(returnCategory[i] not in categoryList):
                         categoryList.append(returnCategory[i])
+
+        # return categoryList
+        # return dataout
         return render_template('search.html', getdata=dataout, dataLength=8, resultLength=-1, categories=categoryList)
     else:
         requestResults = request.form['results']
@@ -27,44 +47,57 @@ def search():
             reader = csv.reader(file)
             search.append(next(reader))
 
-            #I'm working on checking if the products have the correct categories selected
-            request.form.get('categoryselect')
-            request.form.get('stockselect')
-            print(request.form)
-            if 'stock' in request.form:
-                print(request.form.getlist['stock'])
-            else:
-                print('Stock not in form.')
-            if 'category' in request.form:
-                print(request.form.getlist['category'])
-            else:
-                print('Category not in form')
+            # I'm working on checking if the products have the correct categories selected
+            catSelect = request.form.get('categoryselect')
+            stockSelect = request.form.get('stockselect')
+
             categoryList = []
             for line in reader:
-                if (requestResults.lower() in ''.join(line).lower()):
-                    search.append(line)
                 returnCategory = line[len(
                     line) - 1].replace('product_cat-', '').title().split(',')
+                if (requestResults.lower() in ''.join(line).lower()):
+                    if catSelect != None:  # If there is category
+                        if stockSelect != None:  # If there is stock
+                            # Check if product is correct
+                            if catSelect in returnCategory and stockSelect in line[2]:
+                                search.append(line)  # Add line if so
+                        else:  # Else, if there is no stock
+                            if catSelect in returnCategory:  # And if the category is correct
+                                search.append(line)  # Then append the line
+                    else:  # Else, if there is no category
+                        if stockSelect != None:  # But if there is stock
+                            if stockSelect in line[2]:  # Check if stock valid
+                                search.append(line)  # If it is, append line
+                        else:
+                            # Else, if stock and category do not exist, append line
+                            search.append(line)
                 for i in range(0, len(returnCategory)):
                     if(returnCategory[i] not in categoryList):
                         categoryList.append(returnCategory[i])
         return render_template('search.html', getdata=search, dataLength=8, resultLength=len(search) - 1, categories=categoryList)
 
-
-@app.route("/stock", methods=["POST", "GET"])
-def stock():
-    if request.method == 'GET':
-        """convert the csv into a basic html table format"""
-        dataout = []
-        with open("GE_Data.csv", mode='r', encoding='utf-8-sig') as file:
-            reader = csv.reader(file)
-            for line in reader:
-                dataout.append(line)
-        return render_template('stock.html', getdata=dataout, dataLength=8)
+ #Working on making a csv function and beginning early stages of development for editor
+@app.route("/editor", methods=["POST", "GET"])
+def editor():
+    if request.method == "POST":
+        return render_template('editor.html', getdata=dataout, datalength=8)
     else:
-        requestResults = request.form['results']
-        print(requestResults)
-        redirect(url_for('search'), code=307)
+        return render_template('editor.html', getdata=dataout, dataLength=8)
+
+        # @app.route("/stock", methods=["POST", "GET"])
+        # def stock():
+        #     if request.method == 'GET':
+        #         """convert the csv into a basic html table format"""
+        #         dataout = []
+        #         with open("GE_Data.csv", mode='r', encoding='utf-8-sig') as file:
+        #             reader = csv.reader(file)
+        #             for line in reader:
+        #                 dataout.append(line)
+        #         return render_template('stock.html', getdata=dataout, dataLength=8)
+        #     else:
+        #         requestResults = request.form['results']
+        #         print(requestResults)
+        # redirect(url_for('search'), code=307)
 
 
 if __name__ == "__main__":
