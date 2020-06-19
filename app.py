50547c28ff92a0980fdd9ab55daa921a1604f9f0
HatBreakingDad <48573593+HatBreakingDad@users.noreply.github.com>
2020-06-19 18:50:01 +1000

    Completed editor function
    After troubleshooting several issues related to file encoding and reading/writing the file, I was able to get the editor function working. The code for this section was also rewritten, as I was now able to anticipate what steps I would have to undergo to create the algorithm. I also added error checking to certain form values that may return as empty or none. The code worked as follows:
    Fetched and checked the form data
    Organised it into an array to be pushed to the file
    Create a temporary array containing
    
    CSV lines
    Find line with data to replace
    Replace line in temporary array
    Open CSV file in write format
    Write temporary array with new data to CSV
    
    I also added the search bar to the editor page, so that the user can access the same search functions from the editor page.

--- app.py
+++ app.py
@@ -19,17 +19,26 @@ def getCSV():
             for i in range(0, len(returnCategory)):
                 if(returnCategory[i] not in returnCatList):
                     returnCatList.append(returnCategory[i])
-        return returnData, returnCatList[1:]
+        return returnData, sorted(returnCatList[1:])
+
+
+def editorSetup():
+    fileReturn = getCSV()
+    data = fileReturn[0]
+    categoryList = fileReturn[1]
+    IDList = []
+    DictID = {}
+    for i in range(1, len(data)):
+        IDList.append(data[i][0])
+        DictID[data[i][0]] = data[i][1:]
+    return data, sorted(IDList), DictID, categoryList
 
 
 @app.route("/search", methods=["POST", "GET"])
 def search():
     if request.method == 'GET':
-        """convert the csv into a basic html table format"""
-        fileReturn = getCSV()
-        data = fileReturn[0]
-        categoryList = fileReturn[1]
-        return render_template('search.html', getdata=data, dataLength=8, resultLength=-1, categories=categoryList)
+        data = getCSV()
+        return render_template('search.html', getdata=data[0], dataLength=8, resultLength=-1, categories=data[1])
     else:
         requestResults = request.form['results']
         catSelect = request.form.get('categoryselect')
@@ -37,51 +46,33 @@ def search():
         search = []
 
         fileReturn = getCSV()
+        reader = fileReturn[0]
         categoryList = fileReturn[1]
 
-        if (requestResults is "" and catSelect is None and stockSelect is None):
+        if (requestResults == "" and catSelect is None and stockSelect is None):
             search = fileReturn[0]
             return render_template('search.html', getdata=search, dataLength=8, resultLength=len(search) - 1, categories=categoryList)
 
-        with open("GE_Data.csv", mode='r', encoding='utf-8-sig') as file:
-            reader = csv.reader(file)
-            search.append(next(reader))
+        search.append(reader.pop(0))
 
-            for line in reader:
-                if requestResults != "" and requestResults not in ''.join(line):
-                    continue
-                if stockSelect is not None and stockSelect not in line[2]:
-                    continue
-                if catSelect is not None and catSelect not in line[7]:
-                    continue
-                search.append(line)
+        for line in reader:
+            if requestResults != "" and requestResults not in ''.join(line):
+                continue
+            if stockSelect is not None and stockSelect not in line[2]:
+                continue
+            if catSelect is not None and catSelect not in line[7]:
+                continue
+            search.append(line)
 
         return render_template('search.html', getdata=search, dataLength=8, resultLength=len(search) - 1, categories=categoryList)
-
- # Working on making a csv function and beginning early stages of development for editor
 
 
 @app.route("/editor", methods=["POST", "GET"])
 def editor():
     if request.method == "GET":
-        fileReturn = getCSV()
-        data = fileReturn[0]
-        categoryList = fileReturn[1]
-        IDList = []
-        DictID = {}
-        for i in range(1, len(data)):
-            IDList.append(data[i][0])
-            DictID[data[i][0]] = data[i][1:]
-        return render_template('editor.html', getdata=data, IDList=IDList, DictID=DictID, categories=categoryList)
+        templateValues = editorSetup()
+        return render_template('editor.html', getdata=templateValues[0], IDList=templateValues[1], DictID=templateValues[2], categories=templateValues[3])
     else:
-        fileReturn = getCSV()
-        data = fileReturn[0]
-        categoryList = fileReturn[1]
-        IDList = []
-        dataDict = {}
-        for i in range(1, len(data)):
-            IDList.append(data[i][0])
-            dataDict[data[i][0]] = data[i][1:]
         # Request Form Variables
         # ID -> productID
         id = request.form['productID']
@@ -92,44 +83,38 @@ def editor():
         # Image -> image-select-primary
         image = request.form['image-select-primary']
         # Sale Price -> productSale (Need to add $ sign)
-        salePrice = '$' + str(request.form['productSale'])
+        salePrice = ""
+        if request.form['productSale'] != "":
+            salePrice = '$' + str(request.form['productSale'])
         # Normal Price -> productPrice (Need to add $ sign)
-        normalPrice = '$' + str(request.form['productPrice'])
+        normalPrice = ""
+        if request.form['productPrice'] != "":
+            normalPrice = '$' + str(request.form['productPrice'])
         # SKU -> productSKU
         sku = request.form['productSKU']
-        print(sku)
-        # return render_template('editor.html')
-        file = csv.reader(open('GE_Data.csv'))
+
+        for key in request.form:
+            print(request.form[key])
+
+        file = csv.reader(open('GE_Data.csv', encoding="utf-8-sig", newline=''))
         lines = list(file)
 
-        categories = request.form.getlist('productCatSelect')
-        print(categories)
-        print(writeArray=[id, name, stock, image, salePrice, normalPrice, sku, line[7]])
+        writeArray = []
 
-        for line in lines:
-            if id == line[0]:
-                writeArray = []
-                try:
-                    # Select Category -> productCatSelect (We are getting it in a try statement as the user may not have selected any categories)
-                    categories = request.form.getlist('productCatSelect')
-                    print(categories)
-                    if len(categories) == 0:
-                        writeArray = [id, name, stock, image,
-                                      salePrice, normalPrice, sku, line[7]]
-                    else:
-                        writeArray = [id, name, stock, image,
-                                      salePrice, normalPrice, sku, categories]
-                except:
-                    writeArray = [id, name, stock, image,
-                                  salePrice, normalPrice, sku, line[7]]
-                line = writeArray
-                print(line)
-        # with open("GE_Data.csv", mode='a+', encoding='utf-8-sig') as file:
-        #     writer = csv.writer(file)
-        #     writer = csv.writer(file)
-        #     print(csv.writer(file).values)
-        #     writeArray = [id, name, stock, image, salePrice, normalPrice, sku, categories]
-        return "<br>Ok<br>"
+        for i in range(0, len(lines)):
+            if id == lines[i][0]:
+                categories = ','.join(request.form.getlist('productCatSelect'))
+                if len(categories) <= 0:
+                    categories = lines[i][7]
+                writeArray = [id, name, stock, image, salePrice, normalPrice, sku, categories]
+                lines[i] = writeArray
+
+        with open("GE_Data.csv", mode='w+', encoding='utf-8-sig', newline='') as file:
+            writer = csv.writer(file, delimiter=",")
+            writer.writerows(lines)
+
+        templateValues = editorSetup()
+        return render_template('editor.html', getdata=templateValues[0], IDList=templateValues[1], DictID=templateValues[2], categories=templateValues[3])
 
 
 if __name__ == "__main__":
