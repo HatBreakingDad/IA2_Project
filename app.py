35b457df79358c80da1948b5d7b303884e3365fa
HatBreakingDad <48573593+HatBreakingDad@users.noreply.github.com>
2020-07-14 14:01:34 +1000

    Optimised product editing and comment implementation
    Began implementing comments throughout the code so that the code is reusable and readable for future coding additions.
    
    Added a render function for the new home page template, and added several redirects to this page in case the user accidently types in a different link.
    
    In the search function no major changes were made, but some variable names were modified to ensure readability.
    
    In the editor function I removed the now defunct checking if certain fields existed, as the new editor form ensures that all fields have to be filled out before submission.

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 import csv
-import json
 
 from flask import Flask, render_template, request, url_for, redirect
 
-
+# GetData is not used in editor.html template and is unnecessarily returned in the editorSetup function.
 app = Flask(__name__)
+
+"""
+    Description
+    
+    Args:
+        
+    Returns:
+        
+    """
 
 
 def getCSV():
+    # Open the file in read mode using utf-8-sig encoding -> allow for a wider range of characters
     with open("GE_Data.csv", mode='r', encoding='utf-8-sig') as file:
         reader = csv.reader(file)
         returnCatList = []
@@ -17,7 +26,9 @@ def getCSV():
             # We read the product's categories as is, but we later remove the product_cat prefix and dashing to improve user readability
             returnCategory = line[len(line) - 1].split(',')
             for i in range(0, len(returnCategory)):
-                if(returnCategory[i] not in returnCatList):
+                # Creating a category list that will be displayed in the search bar. It checks if a category is already in the list and if
+                # adds it to the list. It also makes sure that blank categories aren't added to the list.
+                if(returnCategory[i] not in returnCatList and returnCategory[i] != ""):
                     returnCatList.append(returnCategory[i])
         return returnData, sorted(returnCatList[1:])
 
@@ -26,21 +37,39 @@ def editorSetup():
     fileReturn = getCSV()
     data = fileReturn[0]
     categoryList = fileReturn[1]
-    IDList = []
-    DictID = {}
+    idList = []
+    dictID = {}
     for i in range(1, len(data)):
-        IDList.append(data[i][0])
-        DictID[data[i][0]] = data[i][1:]
-    return data, sorted(IDList), DictID, categoryList
+        idList.append(data[i][0])
+        dictID[data[i][0]] = data[i][1:]
+    return data, sorted(idList), dictID, categoryList
+
+# Bottom two links redirect to home just in case the user forgets how to get to the homepage
+
+
+@app.route("/index")
+def index():
+    return redirect(url_for('main'))
+
+
+@app.route("/home")
+def home():
+    return redirect(url_for('main'))
+
+
+@app.route("/")
+def main():
+    return render_template('home.html')
 
 
 @app.route("/search", methods=["POST", "GET"])
 def search():
     if request.method == 'GET':
         data = getCSV()
+        # Returning data, length of each row, result length (-1 tells template no responses were found) and list of categories
         return render_template('search.html', getdata=data[0], dataLength=8, resultLength=-1, categories=data[1])
     else:
-        requestResults = request.form['results']
+        searchTerm = request.form['results']
         catSelect = request.form.get('categoryselect')
         stockSelect = request.form.get('stockselect')
         search = []
@@ -49,72 +78,78 @@ def search():
         reader = fileReturn[0]
         categoryList = fileReturn[1]
 
-        if (requestResults == "" and catSelect is None and stockSelect is None):
-            search = fileReturn[0]
-            return render_template('search.html', getdata=search, dataLength=8, resultLength=len(search) - 1, categories=categoryList)
+        if (searchTerm == "" and catSelect is None and stockSelect is None):
+            search = reader
+            return render_template('search.html', getdata=search, dataLength=8, resultLength=-1, categories=categoryList)
 
+        # Adding header row
         search.append(reader.pop(0))
 
+        # The following for loop goes through the current product data and matches it with the criteria provided by the user
         for line in reader:
-            if requestResults != "" and requestResults not in ''.join(line):
+            # If the user did use a search term and the term is not in the result, skip this csv line
+            if searchTerm != "" and searchTerm not in ''.join(line):
                 continue
+            # If the user did use a stock filter and the stock condition was not in the result, skip this csv line
             if stockSelect is not None and stockSelect not in line[2]:
                 continue
+            # If the user did use a category filter and the category was not in the result, skip this csv line
             if catSelect is not None and catSelect not in line[7]:
                 continue
+            # If the compiler has made it here, it means the result fulfils all of the user filters and hence, the csv line is appended to the results list
             search.append(line)
 
-        return render_template('search.html', getdata=search, dataLength=8, resultLength=len(search) - 1, categories=categoryList)
+        return render_template('search.html', getdata=search, dataLength=8, resultLength=len(search) - 1, categories=categoryList, searchTerm=searchTerm)
 
 
 @app.route("/editor", methods=["POST", "GET"])
 def editor():
     if request.method == "GET":
         templateValues = editorSetup()
-        return render_template('editor.html', getdata=templateValues[0], IDList=templateValues[1], DictID=templateValues[2], categories=templateValues[3])
+        return render_template('editor.html', getdata=templateValues[0], idList=templateValues[1], dictID=templateValues[2], categories=templateValues[3], success=0)
     else:
         # Request Form Variables
-        # ID -> productID
         id = request.form['productID']
-        # Name -> productName
         name = request.form['productName']
-        # Stock -> productStock
         stock = request.form['productStock']
-        # Image -> image-select-primary
         image = request.form['image-select-primary']
-        # Sale Price -> productSale (Need to add $ sign)
-        salePrice = ""
-        if request.form['productSale'] != "":
-            salePrice = '$' + str(request.form['productSale'])
-        # Normal Price -> productPrice (Need to add $ sign)
-        normalPrice = ""
-        if request.form['productPrice'] != "":
-            normalPrice = '$' + str(request.form['productPrice'])
-        # SKU -> productSKU
+        # Sale Price -> productSale (Need to add $ sign for CSV)
+        salePrice = '$' + str(request.form['productSale'])
+        # Normal Price -> productPrice (Need to add $ sign for CSV)
+        normalPrice = '$' + str(request.form['productPrice'])
         sku = request.form['productSKU']
 
-        for key in request.form:
-            print(request.form[key])
-
-        file = csv.reader(open('GE_Data.csv', encoding="utf-8-sig", newline=''))
+        file = csv.reader(
+            open('GE_Data.csv', encoding="utf-8-sig", newline=''))
+        # Doing this instead of getCSV function as it is more efficient as I do not need the categories value
         lines = list(file)
 
-        writeArray = []
+        writeList = []
 
         for i in range(0, len(lines)):
             if id == lines[i][0]:
                 categories = ','.join(request.form.getlist('productCatSelect'))
+                # If the user selected no categories, simply use the current ones
                 if len(categories) <= 0:
                     categories = lines[i][7]
-                writeArray = [id, name, stock, image, salePrice, normalPrice, sku, categories]
-                lines[i] = writeArray
+                writeList = [id, name, stock, image,
+                              salePrice, normalPrice, sku, categories]
+                # What we do here is check if any changes were made to the line, if not, we don't bother writing the form submitted line
+                # as it won't make a difference. I did try to implement something like this using js and html, however I was unable to
+                # succesfully make something that would work https://www.sitepoint.com/detect-html-form-changes/ (is what I looked at)
+                if lines[i] == writeList:
+                    templateValues = editorSetup()
+                    return render_template('editor.html', getdata=templateValues[0], idList=templateValues[1], dictID=templateValues[2], categories=templateValues[3], success=1)
+                lines[i] = writeList
 
+        # Now writing all rows (including edited row) back to file. I would like to find a more efficient solution that only replaces
+        # the edited line
         with open("GE_Data.csv", mode='w+', encoding='utf-8-sig', newline='') as file:
             writer = csv.writer(file, delimiter=",")
             writer.writerows(lines)
 
         templateValues = editorSetup()
-        return render_template('editor.html', getdata=templateValues[0], IDList=templateValues[1], DictID=templateValues[2], categories=templateValues[3])
+        return render_template('editor.html', getdata=templateValues[0], idList=templateValues[1], dictID=templateValues[2], categories=templateValues[3], success=2)
 
 
 if __name__ == "__main__":
